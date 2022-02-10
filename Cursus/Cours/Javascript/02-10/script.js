424e470bb84aed72af3c4d6d484451338ed0fe23
Pseudal <Adrien.g@gmx.fr>
2022-02-10 10:49:43 +0100

    tfyfgdh

---
+++ Cursus/Cours/Javascript/02-10/script.js
@@ -0,0 +1,13 @@
+var nom;
+nom = "toto";
+console.log(nom);
+
+alert("hello world");
+
+nom = false;
+console.log (typeof(nom));
+//console.log("hello");
+//console.info("hello");
+//console.debug("hello");
+//console.warn("hello");
+//console.error("hello");
