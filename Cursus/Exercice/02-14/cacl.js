997e77664304ede0750bfa7b9bb5eacaa483435c
Pseudal <adrien.g@gmx.fr>
2022-02-16 09:22:37 +0100

    ex

--- Cursus/Exercice/02-14/cacl.js
+++ Cursus/Exercice/02-14/cacl.js
@@ -1,70 +1,45 @@
-function add(nombre1, nombre2)
-{
-    
-    nombre1 = nombre1 + nombre2;
-    
+function calc(nombre1 = 0, nombre2 = 0, operateur){
+    if(operateur == '+')
+        nombre1 = nombre1 + nombre2
+    else if(operateur == '*'){
+        if(nombre1 == 0|| nombre2 == 0){
+            console.log("Erreur, pas de multiplication par zéro");
+            return("!");}
+        nombre1 = nombre1 * nombre2
+    } 
+    else if(operateur == '-')
+        nombre1 = nombre1 - nombre2
+    else if(operateur == '/'){
+        if(nombre1 == 0|| nombre2 == 0){
+            console.log("Erreur, pas de division par zéro");
+            return("!");}
+        nombre1 = nombre1 / nombre2
+    } 
+    else if(operateur == '%')
+        nombre1 = nombre1 % nombre2;
+    else {
+        console.log("Erreur, votre opérateur est faux")
+        return("!");
+    }
     return(nombre1);
-    
-}
-function mult(nombre1, nombre2)
-{
-    
-    nombre1 = nombre1 * nombre2;
-    
-    return(nombre1); 
-    
-}
-function sous(nombre1, nombre2)
-{
-    
-    nombre1 = nombre1 - nombre2;
-    
-    return(nombre1);
-    
-}
-
-function divi( nombre1, nombre2)
-{
-    
-    nombre1 = nombre1 / nombre2;
-    
-    return(nombre1); 
-    
-}
-function mod( nombre1, nombre2)
-{
-    
-    nombre1 = nombre1 % nombre2;
-    
-    return(nombre1); 
-    
 }
 
 function main()
 {
-    var nombre1 = 5; //parseInt(prompt("1er nombre"));    
-    var operateur = "+"; //prompt("opérateur ? ( / , * , - , + , % )"); 
-    var nombre2 = 3; //parseInt(prompt("2eme nombre"));
+    var nombre1 = 0; //parseInt(prompt("1er nombre"));    
+    var operateur = "-"; //prompt("opérateur ? ( / , * , - , + , % )"); 
+    var nombre2 = 0; //parseInt(prompt("2eme nombre"));
     var res = 0;
 
     if(isNaN(nombre1) || isNaN(nombre2)) {
         console.log("erreur, un des nombres est faux");
         return(0);
     }
+    res = calc(nombre1, nombre2, operateur);
 
-    if(operateur == '+')
-        res = add(nombre1, nombre2);
-    else if(operateur == '*')
-        res = mult(nombre1, nombre2);
-    else if(operateur == '-')
-        res = sous(nombre1, nombre2);
-    else if(operateur == '/')
-        res = divi(nombre1, nombre2);
-    else if(operateur == '%')
-        res = mod(nombre1, nombre2);
-    else{
-        printf("erreur, l'opérateur est faux");
-        return(0); }            
+    if(res == "!")
+        return(0);
+        
     console.log("le resultât de " +nombre1+ " " +operateur+ " " +nombre2+ " = " +res+ ".");        
     return 0;
 }
