0ff5ccb680944d353513f3a70ed07c4494af315a
Pseudal <adrien.g@gmx.fr>
2022-02-28 20:51:30 +0100

    ggh

--- Cursus/Exercice/02-25/js.js
+++ Cursus/Exercice/02-25/js.js
@@ -4,10 +4,10 @@ let result = "";
 let ok = document.querySelector('#ok')
 let error = document.querySelector('#error')
 
-function chercher(){
+ function chercher(){
     let txt = "";
     id = document.querySelector('input').value;
-    for(let i = 0; i < result.colis.length; i++){
+     for(let i = 0; i < result.colis.length; i++){
         if(id == result.colis[i].id){
             txt = `<p>${result.colis[i].status}</p>`
             document.querySelector('#ici').innerHTML = txt;
@@ -15,22 +15,22 @@ function chercher(){
             ok.style.display = "block"
             id = "";
             return;
+            }
         }
-    }
     ok.style.display = "none"
     error.style.display = "block"
 }
 
 function main(){
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', "data.json", true);
-    xhr.send(null);
+     const xhr = new XMLHttpRequest();
+     xhr.open('GET', "data.json", true);
+     xhr.send(null);
 
-    xhr.onreadystatechange  = function(){
-        if(xhr.readyState == 4 && xhr.status == 200){
+     xhr.onreadystatechange  = function(){
+         if(xhr.readyState == 4 && xhr.status == 200){
             result = JSON.parse(xhr.responseText);
             chercher()
-        }
-    }
-}
+         }
+     }
+ }
 button.addEventListener('click', main)
